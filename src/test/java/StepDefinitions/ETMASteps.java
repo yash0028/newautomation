3b5512ec0f27d56089b24940af114d5fc65a4c45
Wacker, Joe J <joe.wacker@optum.com>
2018-05-11 11:15:23 -0500

    Added StepDefinition File for ETMA

---
+++ src/test/java/StepDefinitions/ETMASteps.java
@@ -0,0 +1,29 @@
+package StepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by jwacker on 5/11/2018.
+ */
+public class ETMASteps {
+    @Given("^the connection details$")
+    public void theConnectionDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the micro service calls the ETMA tables$")
+    public void theMicroServiceCallsTheETMATables() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the connection is successful$")
+    public void theConnectionIsSuccessful() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
