c9fe9b4245efa24786b43e09d01b8e9bde72f11e
Wacker, Joe J <joe.wacker@optum.com>
2018-05-11 10:52:57 -0500

    Added TestRunner for PES

---
+++ src/test/java/TestRunnerPES.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jwacker on 4/4/2018.
+ */
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.*;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format={"pretty",
+        "html:target/test-report",
+        "json:target/test-report.json",
+        "junit:target/test-report.xml"},
+        features="src/test/features",
+        glue={"StepDefinitions"},
+        tags = {"not @ignore and @PES and @2018.PI02.01"})
+public class TestRunnerPES {}
