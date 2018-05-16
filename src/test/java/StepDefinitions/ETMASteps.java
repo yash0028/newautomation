ab66b055b3d63a8451e1a3878512750fa25845d9
Wacker, Joe J <joe.wacker@optum.com>
2018-05-16 13:05:08 -0500

    Added StepDefinition outline for ETMA (local testing)

--- src/test/java/StepDefinitions/ETMASteps.java
+++ src/test/java/StepDefinitions/ETMASteps.java
@@ -4,26 +4,40 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+import static org.junit.Assert.assertEquals;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 /**
  * Created by jwacker on 5/11/2018.
  */
 public class ETMASteps {
+    private Response response;
+    private ResponseBody body;
+    private String url;
+
     @Given("^the connection details$")
     public void theConnectionDetails() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        // Testing locally until pushed to Dev env -- need to update this when deployed
+        url = "http://localhost:8080/api/1/markets";
+
     }
 
     @When("^the micro service calls the ETMA tables$")
     public void theMicroServiceCallsTheETMATables() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        response = when().get(url);
+
     }
 
     @Then("^the connection is successful$")
     public void theConnectionIsSuccessful() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        assertEquals(200, response.getStatusCode());
+
     }
 }
