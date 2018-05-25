71e02da15edeae73bfac43a85b4cb1be803953ce
Potluri, Gowtham <gowtham_potluri@optum.com>
2018-05-25 14:15:03 +0000

    Update TestRunnerPES.java
    Updated the formatting structure of the code inside cucumber options

--- src/test/java/TestRunnerPES.java
+++ src/test/java/TestRunnerPES.java
@@ -10,12 +10,12 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/features",
-				glue = { "StepDefinitions" },
-				plugin = { "pretty",
-						"html:target/test-report",
-						"json:target/test-report.json",
-						"junit:target/test-report.xml",
-                		"com.cucumber.listener.ExtentCucumberFormatter:target/PES-report.html"},
-				tags = {"~@ignore", "@PES", "@2018.PI02.01"})
+		 glue = { "StepDefinitions" },
+		 plugin = { "pretty",
+			    "html:target/test-report",
+			    "json:target/test-report.json",
+			    "junit:target/test-report.xml",
+                	    "com.cucumber.listener.ExtentCucumberFormatter:target/PES-report.html"},
+		 tags = {"~@ignore", "@PES", "@2018.PI02.01"})
 public class TestRunnerPES {
 }
