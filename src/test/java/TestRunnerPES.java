03f120b11d21713655e90a31c65f710b820070e5
Wacker, Joe J <joe.wacker@optum.com>
2018-05-25 14:33:17 -0500

    Changed TestRunnerPES to run all PES steps for PI02.02

--- src/test/java/TestRunnerPES.java
+++ src/test/java/TestRunnerPES.java
@@ -11,11 +11,11 @@ import cucumber.api.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/features",
 		 glue = { "StepDefinitions" },
-		 plugin = { "pretty",
-			    "html:target/test-report",
-			    "json:target/test-report.json",
-			    "junit:target/test-report.xml",
-                	    "com.cucumber.listener.ExtentCucumberFormatter:target/PES-report.html"},
-		 tags = {"~@ignore", "@PES", "@2018.PI02.01"})
+		 plugin = {"pretty",
+			       "html:target/test-report-plain-PES",
+			       "json:target/test-report-plain-PES.json",
+			       "junit:target/test-report-plain-PES.xml",
+				    "com.cucumber.listener.ExtentCucumberFormatter:target/PES-report.html"},
+		 tags = {"~@ignore", "@PES", "@2018.PI02.02"})
 public class TestRunnerPES {
 }
