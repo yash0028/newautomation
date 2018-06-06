74d36c3bb83cdff15cf6b4fca6390af99694459e
MS\gpotluri <gowtham_potluri@optum.com>
2018-06-07 01:12:42 +0530

    updated the test-reports folder

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -10,9 +10,9 @@ import cucumber.api.junit.*;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/features",
         	 glue={"StepDefinitions"},
-        	 plugin = {"pretty", "html:target/test-report",
-        	           "json:target/test-report.json",
-        	           "junit:target/test-report.xml",
-                       "com.cucumber.listener.ExtentCucumberFormatter:target/extent-report.html"},
+        	 plugin = {"pretty", "html:target/test-reports/html-cucumber-report",
+        	           "json:target/test-reports/json-report.json",
+        	           "junit:target/test-reports/xml-report.xml",
+                       "com.cucumber.listener.ExtentCucumberFormatter:target/test-reports/html-extent-report.html"},
         	 tags = {"~@ignore","@2018.PI02.01", "@PES,@ETMA"}) //Should be specified in run configuration
 public class TestRunner {}
