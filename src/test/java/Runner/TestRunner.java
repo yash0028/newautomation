0f0bca29a6ed0ce1b3eaaa041d335f240f22c71c
MS\gpotluri <gowtham_potluri@optum.com>
2018-06-07 11:03:18 +0530

    updated test report folder name to have easy access from jenkins job

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -10,9 +10,8 @@ import cucumber.api.junit.*;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/features",
         	 glue={"StepDefinitions"},
-        	 plugin = {"pretty", "html:target/test-reports/html-cucumber-report",
-        	           "json:target/test-reports/json-report.json",
-        	           "junit:target/test-reports/xml-report.xml",
-                       "com.cucumber.listener.ExtentCucumberFormatter:target/test-reports/html-extent-report.html"},
+        	 plugin = {"pretty", "json:target/json-reports/json-report.json",
+        	           "junit:target/xml-reports/xml-report.xml",
+                       "com.cucumber.listener.ExtentCucumberFormatter:target/html-reports/html-report.html"},
         	 tags = {"~@ignore","@2018.PI02.01", "@PES,@ETMA"}) //Should be specified in run configuration
 public class TestRunner {}
