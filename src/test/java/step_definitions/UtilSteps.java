efe7dc020d6f69736009d279aec1e641bc918a52
aberns <alex.berns@optum.com>
2018-08-07 13:28:30 -0500

    Added UtilSteps.java to allow scenario logging

---
+++ src/test/java/step_definitions/UtilSteps.java
@@ -0,0 +1,29 @@
+package step_definitions;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+/**
+ * Created by aberns on 8/6/2018.
+ */
+public class UtilSteps {
+    public static Scenario scenario;
+
+    @Before
+    public void getScenario(Scenario scenario) {
+        System.out.println("Grabbing Scenario Object and waiting for a bit...");
+        this.scenario = scenario;
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void sendRallyUpdate(){
+        //TODO
+    }
+}
