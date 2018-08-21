e4606dc7d61d3e60971ca0c17b2ecdc0d5ec0cbc
Divendra Timaul <divendra.timaul@optum.com>
2018-08-21 14:02:09 -0500

    Finish theResponseIncludesAllRecordsThatMatched method

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,15 +1,30 @@
+import step_definitions.TaxonomyTableSteps;
 import step_definitions.TransactionSteps;
 
 /**
  * Created by dtimaul on 8/8/18.
  */
 public class Main {
+    // The purpose of the class is used to isolate and run a single test.
     public static void main(String[] args) {
         TransactionSteps steps = new TransactionSteps();
+        TaxonomyTableSteps taxonomySteps = new TaxonomyTableSteps();
 
 
         try {
             steps.aBusinessEventIsReceivedByTheEventGatewayThatRequiresAnUpdateToNDB();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        try {
+            taxonomySteps.theUserProvidesTheValueReqValueForReqFieldAndNdbRecValueFor("22", "ndbSpecCD", "O", "ndbRec");
+            //taxonomySteps.theUserProvidesTheOptionalValueOptionalValueForOptionalField("21", "ndbSpecCD");
+            //taxonomySteps.theQueryResponseProvidesTheMostRecentRecordVersionAttributesData();
+            //taxonomySteps.theResponseIncludesAllRecordsThatMatched();
+            taxonomySteps.theQueryResponseReturnsAnError();
+
+
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
