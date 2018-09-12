1973b9db5780d3bc140d7905fdb3a36a762a175a
MS\gpotluri <gowtham_potluri@optum.com>
2018-09-12 11:08:12 +0530

    updated tags

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.*;
 				"pretty",
 				"json:build/json-reports/json-report.json",
 				"junit:build/xml-reports/xml-report.xml"},
-		tags = {"@Automated and (@US1199511 or @US1234077)"})
+		tags = {"@Automated and @US861033"})
 public class TestRunner {
 
 }
