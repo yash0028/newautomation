3b5b080951f89352567ec85d8c9c60a6c5574d7b
Divendra Timaul <divendra.timaul@optum.com>
2018-09-21 10:41:54 -0500

    remove un-needed comments

--- src/test/java/step_definitions/ContractMetadataApiSteps.java
+++ src/test/java/step_definitions/ContractMetadataApiSteps.java
@@ -24,7 +24,6 @@ import static io.restassured.RestAssured.given;
  * Created by dtimaul on 9/11/18.
  */
 public class ContractMetadataApiSteps {
-    //TODO Will be renamed to contract-metadata-api
     private final static String ENDPOINT = "http://localhost:8080";
     private final static String RESOURCE_PRODUCTCODE = "/v1.0/xwalk/product/code/list";
     private final static String CSV_FILE = "/support/ContractDescriptionIDMap.csv";
