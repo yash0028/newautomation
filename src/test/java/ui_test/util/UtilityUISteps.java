57b05dd0dab425d1aa50960f0c97842c9cf0144f
aberns <alex.berns@gmail.com>
2018-10-02 16:49:26 -0500

    removed rogue Selenium Helper call

--- src/test/java/ui_test/util/UtilityUISteps.java
+++ src/test/java/ui_test/util/UtilityUISteps.java
@@ -4,7 +4,6 @@ import cucumber.api.Result;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.BookendOrder;
@@ -15,18 +14,10 @@ public class UtilityUISteps implements IUiStep {
     @Before(value = "@A_UI_Story", order = BookendOrder.UI)
     public void openConnection(Scenario scenario) {
         SauceLabs.reset(scenario.getName());
-
-//        SeleniumHelper.launchBrowser(FileHelper.getInstance().getConfigValue("browserName"));
     }
 
     @After(value = "@A_UI_Story", order = BookendOrder.UI)
     public void closeConnection(Scenario scenario) {
-        WebDriver driver = SeleniumHelper.getWebDriver();
-        if (driver != null) {
-            driver.quit();
-        }
-        //TODO
-
         if (scenario.getStatus() == Result.Type.PASSED) {
             SauceLabs.getInstance().testPassed();
         } else {
