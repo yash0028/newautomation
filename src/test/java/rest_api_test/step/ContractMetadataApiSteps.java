ec16bbf39d9f4e18d29fe32bf840ced52eb06b29
Divendra Timaul <divendra.timaul@optum.com>
2019-01-10 14:49:59 -0600

    Update ContractMetadataApiSteps methods and endpoints.

--- src/test/java/rest_api_test/step/ContractMetadataApiSteps.java
+++ src/test/java/rest_api_test/step/ContractMetadataApiSteps.java
@@ -2,7 +2,6 @@ package rest_api_test.step;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -17,7 +16,6 @@ import util.file.IFileReader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -27,12 +25,9 @@ import static io.restassured.RestAssured.given;
 public class ContractMetadataApiSteps implements IRestStep, IFileReader {
     private final static Logger log = LoggerFactory.getLogger(ContractMetadataApiSteps.class);
 
-    private final static String ENDPOINT = "http://localhost:8080";
-    private final static String RESOURCE_PRODUCTCODE = "/v1.0/xwalk/product/code/list";
-    private final static String CSV_FILE = "/support/US1185585/ContractDescriptionIDMap.csv";
-
+    private final static String ENDPOINT = "https://contract-metadata-api-clm-dev-ui.ocp-ctc-dmz-nonprod.optum.com";
+    private final static String RESOURCE_PRODUCT_CODE = "/v1.0/product_group_codes";
     private RequestSpecification request;
-    private JsonObject requestBody = new JsonObject();
     private Response response;
     private ArrayList<String> productDescriptions;
 
@@ -45,47 +40,35 @@ public class ContractMetadataApiSteps implements IRestStep, IFileReader {
     @And("^using product description from the corresponding \"([^\"]*)\"$")
     public void setSingleProductDescription(String productDesID) throws Throwable {
         productDescriptions = new ArrayList<>();
-
-        // Use product description id to get product description from contractDescriptionIDMap.csv
-        productDescriptions.add(getProductDescription(productDesID));
+        productDescriptions.add(productDesID);
     }
 
     @And("^using product descriptions from the corresponding \"([^\"]*)\" and \"([^\"]*)\"$")
     public void setMultipleProductDescriptions(String productDesID1, String productDesID2) throws Throwable {
         productDescriptions = new ArrayList<>();
-        productDescriptions.add(getProductDescription(productDesID1));
-        productDescriptions.add(getProductDescription(productDesID2));
-
-        System.out.println("Array list for multiple product descriptions:\n" + productDescriptions);
+        productDescriptions.add(productDesID1);
+        productDescriptions.add(productDesID2);
     }
 
     // Make REST call to get product code list
     @When("^exchanging information about the products included or excluded from an Exari contract$")
     public void getProductCodeIdentifier() throws Throwable {
-        JsonArray productDescriptionArr = new JsonArray();
-
-        if (productDescriptions != null) {
-
-            for (String productDescription : productDescriptions) {
-                productDescriptionArr.add(productDescription);
-            }
-        }
-
-        //Request body: {"productDesc": ["desc1", "desc2"]}
-        requestBody.add("productDesc", productDescriptionArr);
-
-        //Create request with request body
-        request = given().baseUri(ENDPOINT).header("Content-Type", "application/json").body(requestBody);
+        this.request = given().baseUri(ENDPOINT).header("Content-Type", "application/json").relaxedHTTPSValidation();
     }
 
     //verify product Code list Was returned For a single ProductDescription
     @Then("^the crosswalk provides the product code identifier of \"([^\"]*)\"$")
     public void verifySingleProductCodeList(String expectedProductCodes) throws Throwable {
-        // Make post request and store response
-        response = request.post(RESOURCE_PRODUCTCODE);
+        // Make GET request and store response
+        response = request.param("productDescriptions", productDescriptions).get(RESOURCE_PRODUCT_CODE);
+        Assert.assertEquals(200, response.getStatusCode());
+
         ArrayList<String> expectedProductCodeArr = new ArrayList<>(Arrays.asList(expectedProductCodes.split(" ")));
 
         JsonElement result = parseJsonElementResponse(response);
+
+        System.out.println("RESPONSE****: " + result.toString());
+
         Assert.assertTrue(result.isJsonArray());
 
         // Check that the result is a valid json array and returns list of product codes
@@ -101,7 +84,7 @@ public class ContractMetadataApiSteps implements IRestStep, IFileReader {
     @Then("^the crosswalk provides the product code identifiers of \"([^\"]*)\" and \"([^\"]*)\"$")
     public void verifyMultipleProductCodeList(String expectedProductCodes1, String expectedProductCodes2) throws Throwable {
         // Make post request and store response
-        response = request.post(RESOURCE_PRODUCTCODE);
+        response = request.post(RESOURCE_PRODUCT_CODE);
 
         ArrayList<String> expectedProductCodeArr1 = new ArrayList<>(Arrays.asList(expectedProductCodes1.split(" ")));
         ArrayList<String> expectedProductCodeArr2 = new ArrayList<>(Arrays.asList(expectedProductCodes2.split(" ")));
@@ -133,7 +116,7 @@ public class ContractMetadataApiSteps implements IRestStep, IFileReader {
 
     @Then("^the crosswalk provides an empty list$")
     public void theCrosswalkProvidesAnEmptyList() throws Throwable {
-        response = request.post(RESOURCE_PRODUCTCODE);
+        response = request.post(RESOURCE_PRODUCT_CODE);
         JsonElement result = parseJsonElementResponse(response);
 
         Assert.assertTrue(result.isJsonArray());
@@ -144,9 +127,7 @@ public class ContractMetadataApiSteps implements IRestStep, IFileReader {
     @Given("^a product description from the corresponding \"([^\"]*)\" and an invalid product description$")
     public void aProductDescriptionFromTheCorrespondingAndAnInvalidProductDescription(String productDesID1) throws Throwable {
         productDescriptions = new ArrayList<>();
-
-        // Use product description id to get product description from contractDescriptionIDMap.csv
-        productDescriptions.add(getProductDescription(productDesID1));
+        productDescriptions.add(productDesID1);
     }
 
     @Then("^the crosswalk only provides the product code identifier of \"([^\"]*)\" for the valid product description$")
@@ -154,7 +135,7 @@ public class ContractMetadataApiSteps implements IRestStep, IFileReader {
         ArrayList<String> expectedProductCodeArr = new ArrayList<>(Arrays.asList(expectedProductCodes.split(" ")));
 
         // Make post request and store response
-        response = request.post(RESOURCE_PRODUCTCODE);
+        response = request.post(RESOURCE_PRODUCT_CODE);
         JsonElement result = parseJsonElementResponse(response);
         Assert.assertTrue(result.isJsonArray());
 
@@ -208,24 +189,24 @@ public class ContractMetadataApiSteps implements IRestStep, IFileReader {
         return resultProductCodeArr.getAsJsonArray();
     }
 
-    /**
-     * Takes in a product description id, then searches in the csv file for the product description that corresponds to
-     * that id.
-     *
-     * @param id product description id
-     * @return product description
-     */
-    private String getProductDescription(String id) {
-        List<String> lines = getFileLines(CSV_FILE);
-
-        for (String line : lines) {
-            String[] currentLine = line.split("\\|");
-
-            if (currentLine[0].trim().equals(id)) {
-                return currentLine[1].trim();
-            }
-        }
-        return "";
-    }
+//    /**
+//     * Takes in a product description id, then searches in the csv file for the product description that corresponds to
+//     * that id.
+//     *
+//     * @param id product description id
+//     * @return product description
+//     */
+//    private String getProductDescription(String id) {
+//        List<String> lines = getFileLines(CSV_FILE);
+//
+//        for (String line : lines) {
+//            String[] currentLine = line.split("\\|");
+//
+//            if (currentLine[0].trim().equals(id)) {
+//                return currentLine[1].trim();
+//            }
+//        }
+//        return "";
+//    }
 
 }
