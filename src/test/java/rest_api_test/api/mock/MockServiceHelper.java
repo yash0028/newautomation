e0b225a118def8e70c0fb9d87a783960dea92cd1
aberns <alex.berns@gmail.com>
2019-04-08 09:46:18 -0500

    added missing AbstractApi to MockServiceHelper

--- src/test/java/rest_api_test/api/mock/MockServiceHelper.java
+++ src/test/java/rest_api_test/api/mock/MockServiceHelper.java
@@ -2,13 +2,15 @@ package rest_api_test.api.mock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rest_api_test.api.AbstractRestApi;
 
 import static io.restassured.RestAssured.given;
 
-class MockServiceHelper {
+class MockServiceHelper extends AbstractRestApi {
     private static final Logger log = LoggerFactory.getLogger(MockServiceHelper.class);
 
-    private static final String ENDPOINT = "http://cosmos-ndb-mock-service-clm-test.ocp-ctc-dmz-nonprod.optum.com";
+    private static final String ENDPOINT_DEV = "http://cosmos-ndb-mock-service-clm-dev.ocp-ctc-dmz-nonprod.optum.com";
+    private static final String ENDPOINT_TEST = "http://cosmos-ndb-mock-service-clm-test.ocp-ctc-dmz-nonprod.optum.com";
     private static final String RESOURCE_CLEAR_COSMOS_DB = "/v1.0/cosmos/cleardb";
     private static final String RESOURCE_CLEAR_NDB_DB = "/v1.0/ndb/cleardb";
 
@@ -34,17 +36,22 @@ class MockServiceHelper {
     */
 
     public void clearNdb() {
-        given().baseUri(ENDPOINT).delete(RESOURCE_CLEAR_NDB_DB);
+        given().baseUri(getEndpoint()).delete(RESOURCE_CLEAR_NDB_DB);
     }
 
     public void clearCosmos() {
-        given().baseUri(ENDPOINT).delete(RESOURCE_CLEAR_COSMOS_DB);
+        given().baseUri(getEndpoint()).delete(RESOURCE_CLEAR_COSMOS_DB);
     }
     
     /*
     HELPER METHODS
     */
-    
+
+    @Override
+    protected String getEndpoint() {
+        return ENDPOINT_TEST;
+    }
+
     /*
     UTILITY CLASS
     */
