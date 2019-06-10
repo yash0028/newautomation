85a55b0b5ec625ac1b6fa4b098ee2250f2b805b5
aberns <alex.berns@gmail.com>
2019-06-10 15:04:18 -0500

    added hive to configuration loader

---
+++ src/test/java/util/configuration/ConfigStub.java
@@ -0,0 +1,24 @@
+package util.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigStub implements IConfigurable {
+    private static final Logger log = LoggerFactory.getLogger(ConfigStub.class);
+
+    /*
+    CONSTRUCTOR
+    */
+    
+    /*
+    CLASS METHODS
+    */
+    
+    /*
+    HELPER METHODS
+    */
+    
+    /*
+    UTILITY CLASS
+    */
+}
