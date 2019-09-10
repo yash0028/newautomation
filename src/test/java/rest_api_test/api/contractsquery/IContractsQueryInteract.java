215189b43582ae24a8b2ec4c82b78f200b26f6b9
Alex Berns <alex.berns@optum.com>
2019-09-10 15:05:58 -0500

    updated contract query interact to use new pattern

--- src/test/java/rest_api_test/api/contractsquery/IContractsQueryInteract.java
+++ src/test/java/rest_api_test/api/contractsquery/IContractsQueryInteract.java
@@ -1,5 +1,6 @@
 package rest_api_test.api.contractsquery;
 
+import com.google.gson.JsonElement;
 import io.restassured.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,7 @@ public interface IContractsQueryInteract extends IRestApi {
     Logger log = LoggerFactory.getLogger(IContractsQueryInteract.class);
 
     String RESOURCE_ECM = "/v1.0/exari/ecm";
-    String RESOURCE_FACILITY = "/v1.0/exari/facilitycontracts";
+    String RESOURCE_FACILITY_CONTRACTS = "/v1.0/exari/facilitycontracts";
     String RESOURCE_CONTRACT_JSON = "/v1.0/exari/json";
     String RESOURCE_CONTRACT_SEARCH = "/v1.0/exari/contracts/search";
     String RESOURCE_MASS_ACTION = "/v1.0/exari/mass-action";
@@ -25,6 +26,36 @@ public interface IContractsQueryInteract extends IRestApi {
     */
 
     /**
+     * @param contractId the contract id
+     * @return the Response from the Contracts Query api
+     */
+    default Response getExariContractModel(String contractId) {
+        ParamMap params = new ParamMap();
+        params.put("contractId", contractId);
+        return ContractsQueryHelper.getInstance().doParamGet(RESOURCE_ECM, params);
+    }
+
+    /**
+     * @param contractId the contract id
+     * @return the Response from the Contracts Query api
+     */
+    default Response getFacilityContractData(String contractId) {
+        ParamMap params = new ParamMap();
+        params.put("contractId", contractId);
+        return ContractsQueryHelper.getInstance().doParamGet(RESOURCE_FACILITY_CONTRACTS, params);
+    }
+
+    /**
+     * @param contractId the contract id
+     * @return the Response from the Contracts Query api
+     */
+    default Response getExariContractJson(String contractId) {
+        ParamMap params = new ParamMap();
+        params.put("contractId", contractId);
+        return ContractsQueryHelper.getInstance().doParamGet(RESOURCE_CONTRACT_JSON, params);
+    }
+
+    /**
      * @param massActionProjectId the mass action project id
      * @return the Query Response from the Contracts Query api
      */
@@ -35,6 +66,22 @@ public interface IContractsQueryInteract extends IRestApi {
         return ContractsQueryHelper.getInstance().getExariMassAction(response);
     }
 
+    /**
+     * @param searchFilters search by this filter
+     * @return the Response from the Contracts Query api
+     */
+    default Response searchContractsWithFilter(JsonElement searchFilters) {
+        return ContractsQueryHelper.getInstance().doBasicPost(RESOURCE_CONTRACT_SEARCH, searchFilters);
+    }
+
+    /**
+     * @return the Response from the Contracts Query api
+     */
+    default Response searchContractsById(String contractId) {
+        ParamMap params = new ParamMap();
+        params.put("contractId", contractId);
+        return ContractsQueryHelper.getInstance().doParamGet(RESOURCE_CONTRACT_SEARCH, params);
+    }
     
     /*
     UTILITY CLASS
