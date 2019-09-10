c26b1c1cc21cfb3b922f251cd07c836eac4559cc
Alex Berns <alex.berns@optum.com>
2019-09-10 12:49:05 -0500

    added commonly used methods to abstract rest api class

--- src/test/java/rest_api_test/api/AbstractRestApi.java
+++ src/test/java/rest_api_test/api/AbstractRestApi.java
@@ -2,19 +2,25 @@ package rest_api_test.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import general_test.util.ISharedValueReader;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rest_api_test.api.datastructure.gson.contractsquery.QueryResponse;
 import rest_api_test.api.datastructure.gson.transaction.TransactionStatus;
 import rest_api_test.api.datastructure.list.TransactionDetails;
 
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
 public abstract class AbstractRestApi implements ISharedValueReader {
     private static final Logger log = LoggerFactory.getLogger(AbstractRestApi.class);
-    protected final Gson gson;
-
     protected static boolean useDev = true;
+    protected final Gson gson;
 
     /*
     CONSTRUCTOR
@@ -38,6 +44,35 @@ public abstract class AbstractRestApi implements ISharedValueReader {
     /*
     CLASS METHODS
     */
+
+    public Response doBasicGet(String resourceEndpoint) {
+        RequestSpecification request = given().baseUri(getEndpoint());
+
+        // Get the GET response
+        return request.get(resourceEndpoint);
+    }
+
+    public Response doParamGet(String endpoint, Map<String, String> params) {
+        RequestSpecification request = given().baseUri(getEndpoint())
+                .header("Content-Type", "application/json");
+
+        // Add params
+        params.forEach(request::param);
+
+        // Get the GET response
+
+        return request.get(endpoint);
+    }
+
+
+    public Response doBasicPost(String resourceEndpoint, JsonElement payload) {
+        RequestSpecification request = given().baseUri(getEndpoint())
+                .header("Content-Type", "application/json")
+                .body(payload);
+
+        // Get the POST response
+        return request.post(resourceEndpoint);
+    }
     
     /*
     HELPER METHODS
