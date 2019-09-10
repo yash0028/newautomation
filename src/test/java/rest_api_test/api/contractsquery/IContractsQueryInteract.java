f158a060391474a94776734f726ee9e849f2723c
Alex Berns <alex.berns@optum.com>
2019-09-10 14:38:49 -0500

    updated contract query interact to use new pattern

--- src/test/java/rest_api_test/api/contractsquery/IContractsQueryInteract.java
+++ src/test/java/rest_api_test/api/contractsquery/IContractsQueryInteract.java
@@ -1,12 +1,20 @@
 package rest_api_test.api.contractsquery;
 
+import io.restassured.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rest_api_test.api.IRestApi;
+import rest_api_test.api.ParamMap;
 import rest_api_test.api.contractsquery.model.QueryResponse;
 
 public interface IContractsQueryInteract extends IRestApi {
     Logger log = LoggerFactory.getLogger(IContractsQueryInteract.class);
+
+    String RESOURCE_ECM = "/v1.0/exari/ecm";
+    String RESOURCE_FACILITY = "/v1.0/exari/facilitycontracts";
+    String RESOURCE_CONTRACT_JSON = "/v1.0/exari/json";
+    String RESOURCE_CONTRACT_SEARCH = "/v1.0/exari/contracts/search";
+    String RESOURCE_MASS_ACTION = "/v1.0/exari/mass-action";
     
     /*
     INTERFACE METHODS
@@ -16,8 +24,15 @@ public interface IContractsQueryInteract extends IRestApi {
     DEFAULT METHODS
     */
 
+    /**
+     * @param massActionProjectId the mass action project id
+     * @return the Query Response from the Contracts Query api
+     */
     default QueryResponse getExariMassAction(String massActionProjectId) {
-        return ContractsQueryHelper.getInstance().getExariMassAction(massActionProjectId);
+        ParamMap params = new ParamMap();
+        params.put("massActionProjectId", massActionProjectId);
+        Response response = ContractsQueryHelper.getInstance().doParamGet(RESOURCE_MASS_ACTION, params);
+        return ContractsQueryHelper.getInstance().getExariMassAction(response);
     }
 
     
