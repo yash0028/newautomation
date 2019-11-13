303e258b71ddc39e1439fa38c30d96f1da8e312e
Jacob, Leo C <jacob_leo@optum.com>
2019-11-13 20:33:55 +0530

    fixed

--- src/test/java/ui_test/util/LocalDriver.java
+++ src/test/java/ui_test/util/LocalDriver.java
@@ -78,7 +78,7 @@ public class LocalDriver implements IConfigurable, IFileReader {
     private void init() {
         try {
 
-              //String driverPath = "C:\\ProgramData\\Chrome_driver_77.0.3865.40\\chromedriver.exe";
+            //String driverPath = "C:\\ProgramData\\Chrome_driver_77.0.3865.40\\chromedriver.exe";
             //String driverPath = "C:\\ProgramData\\Chrome_driver\\chromedriver.exe";
             String driverPath = getDriverPath();
 
