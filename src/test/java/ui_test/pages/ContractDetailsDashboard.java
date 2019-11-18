e36d147d32606643ba0ab688add939a562563f03
Jacob, Leo C <jacob_leo@optum.com>
2019-11-18 23:12:29 +0530

    open active workflow fix

--- src/test/java/ui_test/pages/ContractDetailsDashboard.java
+++ src/test/java/ui_test/pages/ContractDetailsDashboard.java
@@ -59,7 +59,11 @@ public class ContractDetailsDashboard extends GenericInputPage implements IUiSte
             getActivityManager(false, tierApproval);
         }
     }
-
+    public void openActiveWorkFlow(){
+                if(this.elements.openActiveWorkFlow.getAttribute("class").contains("alfresco-twister-closed")){
+                    click("Active Workflows",this.elements.openActiveWorkFlow);
+                }
+    }
     public boolean getActiveWorkFlow(boolean tierApproval, String location, HashMap<String, String> hmap) {
         int count = 1;
         boolean foundActiveWorkFlow = false;
@@ -69,6 +73,7 @@ public class ContractDetailsDashboard extends GenericInputPage implements IUiSte
                 dontSkip = false;
                 break;
             }
+            openActiveWorkFlow();
             if (CommonMethods.isElementPresent(getDriver(), By.xpath(elements.activeWorkFlow))) {
                 if (click("Active Work Flow Link", elements.activeWorkFlowLink)) {
                     foundActiveWorkFlow = true;
@@ -421,6 +426,8 @@ public class ContractDetailsDashboard extends GenericInputPage implements IUiSte
         private WebElement inTaskApp;
         @FindBy(xpath = "//div[@title='Home']")
         public WebElement headerTabHome;
+        @FindBy(xpath = "//h2[contains(.,'Active Workflows')]")
+        public WebElement openActiveWorkFlow;
 
         private String error = "//div[contains(@class,'alf-error-header')]";
         private String spinner = "//mat-progress-spinner";
