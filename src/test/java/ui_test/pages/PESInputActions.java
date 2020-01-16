2770cc0a60eced148181a37bffc9de06321904df
Jacob, Leo C <jacob_leo@optum.com>
2020-01-16 16:16:40 +0530

    latest code

--- src/test/java/ui_test/pages/PESInputActions.java
+++ src/test/java/ui_test/pages/PESInputActions.java
@@ -27,6 +27,7 @@ public class PESInputActions extends GenericInputPage {
 
     public void enterPESInput(HashMap<String, String> readFile) {
         long startTime = TimeKeeper.getInstance().getCurrentMillisecond();
+
         String[] heading = this.elements.heading.getText().trim().split("-");
         String contractNumber = heading[1].trim().split(" ")[0];
         IWebInteract.log.info("CONTARCT NUMBER : {}", contractNumber);
