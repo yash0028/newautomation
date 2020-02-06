23195bfac06fed51e0cfe946d369c712b9521beb
Jacob, Leo C <jacob_leo@optum.com>
2020-02-06 21:37:53 +0530

    error fixes

--- src/test/java/ui_test/pages/PaymentAppendix.java
+++ src/test/java/ui_test/pages/PaymentAppendix.java
@@ -29,10 +29,8 @@ public class PaymentAppendix extends GenericInputPage {
             Assert.assertTrue(click("High Fee Schedule than Default", paymentAppendixElement(hmap.get("High Fee Schedule than Default"))));
         waitForElementToDissapear(getDriver(), waitForElementToAppear(getDriver(), By.xpath(elements.message)));
 
-        System.out.println("Payment Appendix Include"+isVisible(elements.PaymentAppendixToInclude));
-        if(!isVisible(elements.PaymentAppendixToInclude))
-        {
-            System.out.println("Include Payment Appendix not Present");
+        if(!CommonMethods.isElementPresent(getDriver(),By.xpath(elements.PaymentAppendixToIncludeXpath))){
+            IWebInteract.log.info("[NOT FOUND]: Include Payment Appendix");
             Assert.assertTrue(clickNext());
             Assert.assertTrue(waitForPageLoad());
         }
@@ -95,6 +93,10 @@ public class PaymentAppendix extends GenericInputPage {
             pageExist = true;
         }
 
+        if (CommonMethods.isElementPresent(getDriver(), By.xpath(getFeeSchedule("Mississippi CHIP")))) {
+            Assert.assertTrue(sendKeys("Mississippi CHIP", getFeeScheduleElement("Mississippi CHIP"), hmap.get("FS All Payer Physician")));
+            pageExist = true;
+        }
 
         if (pageExist) {
             Assert.assertTrue(clickNext());
@@ -103,7 +105,7 @@ public class PaymentAppendix extends GenericInputPage {
 
         enterMedicaidCHIPPaymentAppendix(hmap);
 
-       if(multiPage){
+       if(multiPage && pageExist){
            verifyFeeScheduleID(hmap);
        }
     }
@@ -326,7 +328,7 @@ public class PaymentAppendix extends GenericInputPage {
     }
 
     public WebElement getFeeScheduleElement(String allpayerType) {
-        return findElement(getDriver(), new String[]{"xpath", "//label/b[contains(.,'" + allpayerType + "')]/../../../..//input[contains(@name,'Fee_Schedule_Name')]"});
+        return findElement(getDriver(), new String[]{"xpath", "//label/b[contains(.,'" + allpayerType + "')]/../../../..//input[contains(@name,'Fee_Schedule_')]"});
     }
 
     public WebElement getRateEscalator(String ans) {
@@ -334,7 +336,7 @@ public class PaymentAppendix extends GenericInputPage {
     }
 
     public String getFeeSchedule(String allpayerType) {
-        return "//label/b[contains(.,'" + allpayerType + "')]/../../../..//input[contains(@name,'Fee_Schedule_Name')]";
+        return "//label/b[contains(.,'" + allpayerType + "')]/../../../..//input[contains(@name,'Fee_Schedule_')]";
     }
 
     public String getQn(String question) {
@@ -496,7 +498,10 @@ public class PaymentAppendix extends GenericInputPage {
 
                 Assert.assertTrue(clickNext());
                 Assert.assertTrue(waitForPageLoad());
-                enterFeeScheduleID(hmap,true);
+                if(subtopic.equals("Medicaid MGA")){
+                    enterFeeScheduleID(hmap,true);
+                }
+
             }
         }
     }
@@ -532,8 +537,6 @@ public class PaymentAppendix extends GenericInputPage {
 
         @FindBy(xpath=" //input[contains(@name,'MS_CHIP_Fee_Schedule_ID')]")
         private WebElement feeScheduleID_MS;
-
-        //input[contains(@name,'MS_CHIP_Fee_Schedule_ID')]
         @FindBy(xpath = "//span[@class='select2-results']//li")
         private List<WebElement> selectCode;
         @FindBy(xpath="//input[contains(@value,'Standard')]")
@@ -558,11 +561,7 @@ public class PaymentAppendix extends GenericInputPage {
         private String PA_topic = "//p[contains(@class,'topic')]/a[contains(@name,'Payment Appendix')]";
         private String PaymentAppendixStructureStandard = "//input[contains(@value,'Standard')]";
         private String topicPA = "//div[contains(@class,'topicArea')]/p[contains(.,'Payment Appendix')]";
-        // @FindBy(xpath = "//div[contains(text(),'You must select at least one answer']")
-        // private WebElement errormessage;
-        // @FindBy(xpath = "//a[@title='go back one interview round']")
-//        //private WebElement backbutton;
-//        private String selectAll = "//a[contains(.,'Select All')]";
+        private String PaymentAppendixToIncludeXpath = "//b[contains(.,'Payment Appendix to include')]";
 
         public PageElements(SearchContext context) {
             super(context);
