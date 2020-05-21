bd4e387e648fb9ae54d309a7629ca92cdf1894d0
yash <yashg0028@gmail.com>
2020-05-21 08:24:27 +0530

    new

---
+++ Normal/programmes/src/StringMan/stsplit.java
@@ -0,0 +1,23 @@
+package StringMan;
+import java.util.Scanner;
+
+public class stsplit {
+
+	public static void main(String[] args) {
+	
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter your String");
+		String str = in.nextLine();
+	     in.close();
+		String[] token = str.split("");
+		System.out.print(token);
+		//used split method to print in reverse order
+		for(int i=token.length-1; i>=0; i--)
+		{
+			System.out.print(token[i]+ "");
+		}
+		
+	}
+
+}
+	
