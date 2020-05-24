efced890d3b19c35c65c4281fe9afe81d5382b91
yash <yashg0028@gmail.com>
2020-05-24 15:42:53 +0530

    new

--- Normal/programmes/src/advanced/LargestNumber.java
+++ Normal/programmes/src/advanced/LargestNumber.java
@@ -1,11 +1,16 @@
 package advanced;
 
-	public class LargestNumber
+import java.util.Arrays;
+
+public class LargestNumber
 	{
 		public static void main(String args[])
 		{
 			int[] a =  { 20, 30, 50, 49, 71, 100};
-			int max = a[0];
+			
+			Arrays.sort(a);
+			System.out.println(a[5]);
+			/*int max = a[0];
 			
 			System.out.println("The Given Array Element is:");
 			for(int i = 0; i < a.length;i++)
@@ -21,8 +26,8 @@ package advanced;
 			}
 			
 			System.out.println("From The Array Element Largest Number is:" + max);
-		}
-	}
+		}*/
+	}}
 	
 	
 
