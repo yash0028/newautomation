d580418c3fff3853c7b086c29407647b76d9e391
yash <yashg0028@gmail.com>
2020-05-24 15:06:08 +0530

    duplo

---
+++ Normal/programmes/src/StringMan/Dupl.java
@@ -0,0 +1,26 @@
+package StringMan;
+
+public class Dupl 
+{
+	 public static void main(String args[]) 
+	 {
+
+	
+	String str = "w3schools";
+	  int cnt = 0;
+	  char[] chr = str.toCharArray();
+	  System.out.println("Duplicate Characters are:");
+	  for (int i = 0; i < str.length(); i++) 
+	  {
+	   for (int j = i + 1; j < str.length(); j++)
+	   {
+	    if (chr[i] == chr[j]) 
+	    {
+	     System.out.println(chr[j]);
+	     cnt++;
+	     break;
+	    }
+	   }
+	  }
+	  
+}}
