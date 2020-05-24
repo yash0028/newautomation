a92a39328854875b50ef861cd58d2190026fcbe1
yash <yashg0028@gmail.com>
2020-05-24 15:22:27 +0530

    new

---
+++ Normal/programmes/src/StringMan/Remove.java
@@ -0,0 +1,14 @@
+package StringMan;
+
+public class Remove {
+	public static void main(String[] args) {
+		String str = "abcdDCBA123";
+
+		System.out.println("String after Removing 'a' = "+str.replace("a", ""));
+		
+		System.out.println("String after Removing First 'a' = "+str.replaceFirst("ab", ""));
+
+		System.out.println("String after replacing all small letters = "+str.replaceAll("([a-z])", ""));
+	}
+
+}
