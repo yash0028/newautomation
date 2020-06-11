83ae505db9a4bf0f442a1a5cf601acc646c8482d
yash <yashg0028@gmail.com>
2020-06-11 14:32:55 +0530

    new

---
+++ Normal/programmes/src/Arrays/InsertEle.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class InsertEle {
+
+	public static void main(String[] args) {
+
+		int a[]={10,20,30,40,50}; // Array size cannot be increased, So last ele will be deleted if we 
+								 // insert new element in middle
+		int position = 4;
+		int element = 35;
+		
+		for(int i=a.length-1; i>position-1 ; i--)
+		{
+			a[i]=a[i-1];
+		}
+		a[position-1] = element;
+		
+		System.out.println(Arrays.toString(a));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
