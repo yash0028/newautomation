4476cad551bc110437afd838039908658706de5e
yash <yashg0028@gmail.com>
2020-06-29 18:59:29 +0530

    n

--- Normal/programmes/src/excel/StringReverseFor.java
+++ Normal/programmes/src/excel/StringReverseFor.java
@@ -19,7 +19,7 @@ public class StringReverseFor {
 		System.out.println("Reverse order:");
 		for(int i=c.length-1; i>=0; i--) 
 		{
-			reverse = reverse + original.charAt(i);   //used inbuilt method charAt() to reverse the string
+			reverse = reverse + c[i];  
 		}
 		System.out.println(reverse);
 		
