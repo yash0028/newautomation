a57ea5a3d6fb9514afd6fb1f553329bdf5f4f46a
yash yashautomation0028@gmail.com <yash yashautomation0028@gmail.com>
2020-07-12 16:52:31 +0530

    sss

--- Normal/programmes/src/advanced/LargestAndSecondLargest.java
+++ Normal/programmes/src/advanced/LargestAndSecondLargest.java
@@ -25,3 +25,31 @@ public class LargestAndSecondLargest
 	}
 
 }
+
+/*
+ * public static void main(String args[])
+ * { 
+ * int temp, size; 
+ * int array[] = 
+ * {10,20, 25, 63, 96, 57}; 
+ * size = array.length;
+ * 
+ * for(int i = 0; i<size; i++ )
+ * { 
+ * for(int j = i+1; j<size; j++)
+ * {
+ * if(array[i]>array[j])
+ * { 
+ * temp = array[i];
+ *  array[i] = array[j];
+ *   array[j] = temp;
+ * }
+ *  }
+ *   }
+ *       
+ *    System.out.println("First largest number is:: "+array[size-1]);
+
+ *    System.out.println("Second largest number is:: "+array[size-2]);
+ * } 
+ * }
+ */
