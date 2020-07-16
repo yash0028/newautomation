85eb0006515fc40a42bd96c6d2b9f5e9930628d4
yash yashautomation0028@gmail.com <yash yashautomation0028@gmail.com>
2020-07-16 09:15:19 +0530

    mmm

--- Normal/programmes/src/comparators/MapsSortValue.java
+++ Normal/programmes/src/comparators/MapsSortValue.java
@@ -35,12 +35,12 @@ public class MapsSortValue {
 		 * { 
 		 * Map<Integer,String> map=new HashMap<Integer,String>();
 		 *  map.put(100,"Amit");
-		 *   map.put(101,"Vijay");
-		 * map.put(102,"Rahul"); //Returns a Set view of the mappings contained in this
-		 * map map.entrySet() //Returns a sequential Stream with this collection as its source 
+		 *  map.put(101,"Vijay");
+		 * map.put(102,"Rahul"); //Returns a Set view of the mappings contained in this map
+		 * map.entrySet() //Returns a sequential Stream with this collection as its source 
 		 * .stream() //Sorted according to the provided Comparator
-		 * .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) //Performs an
-		 * action for each element of this stream
+		 * .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) //Performs an action for each element of this stream
+		 
 		 *  .forEach(System.out::println); } }
 		 * 
 		 * Output:
--- Normal/programmes/src/comparators/MapsortKey.java
+++ Normal/programmes/src/comparators/MapsortKey.java
@@ -34,8 +34,8 @@ public class MapsortKey {
  * { Map<Integer,String> map=new HashMap<Integer,String>(); 
  * 			map.put(100,"Amit"); 
  * 			map.put(101,"Vijay");
- * 				map.put(102,"Rahul"); //Returns a Set view of the mappings contained in this
- * 				map map.entrySet() //Returns a sequential Stream with this collection as its source 
+ * 				map.put(102,"Rahul"); //Returns a Set view of the mappings contained in this map 
+ *              map.entrySet() //Returns a sequential Stream with this collection as its source 
  * 				.stream() //Sorted according to the provided Comparator
  * 				.sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())) //Performs an action for each element of this stream 
  * 				.forEach(System.out::println);
